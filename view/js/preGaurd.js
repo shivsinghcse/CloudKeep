bb3339c15fdd1520da770294c4e928e7268045ce
shivsinghcse <shivsinghcse@gmail.com>
2026-04-02 12:15:40 +0530

    feat display user details

--- view/js/preGaurd.js
+++ view/js/preGaurd.js
@@ -2,9 +2,12 @@ const checkAuth = async () => {
     try
     {
         const session = await getSession()
+        console.log(session);
+
         if(!session){
             location.href = '/login'
         } 
+        showUserDetails(session)
     }
     catch(err)
     {
@@ -14,3 +17,14 @@ const checkAuth = async () => {
 }
 
 checkAuth()
+
+const showUserDetails = (user) => {
+    const profilePicture = document.getElementById('userImage')
+    const name = document.getElementById('userName')
+    const email = document.getElementById('userEmail')
+
+    profilePicture.src = user.image ?? '../images/avt.png'
+    name.textContent = user.fullname ?? 'Amelia Harper'
+    email.textContent = user.email ?? 'amelia@harper.com'
+    
+}
